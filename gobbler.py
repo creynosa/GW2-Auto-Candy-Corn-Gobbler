7eb3358f2cc653ab2368b5054955d5066b4741e0
Cesar <creynosa@protonmail.com>
2022-01-09 13:20:28 -0600

    Remove elevate from program and confined to only the candy corn gobbler

--- gobbler.py
+++ gobbler.py
@@ -2,63 +2,47 @@
 
 import math
 from pathlib import Path
+from typing import Union
 
 import pyautogui
-from elevate import elevate
 
 
 def printInstructions() -> None:
     """Prints a set of basic instructions on how to use this program."""
-    print('\n======================================================================')
-    text = "To avoid any issues when using this program, do the following:\n\n" \
-           "1. You'll need Guild Wars 2 to be on your main display.\n" \
-           "2. Your resources must be visible somewhere on your screen at all times.\n" \
-           "3. Avoid moving the mouse unless it is to exit this console window.\n" \
-           "4. Feel free to move this window to any other monitor.\n" \
-           "5. A message will appear once this program has finished running.\n\n" \
-           "Press enter to continue: "
+    print("\n======================================================================")
+    text = (
+        "To avoid any issues when using this program, do the following:\n\n"
+        "1. You'll need Guild Wars 2 to be on your main display.\n"
+        "2. Your resources must be visible somewhere on your screen at all times.\n"
+        "3. Avoid moving the mouse unless it is to exit this console window.\n"
+        "4. Feel free to move this window to any other monitor.\n"
+        "5. A message will appear once this program has finished running.\n\n"
+        "Press enter to continue: "
+    )
 
     input(text)
-
-
-def askForGobbler() -> str:
-    """Asks the user what resources the program will be using and returns the name of the resources."""
-    gobblers = {
-        1: 'candy-corn',
-        2: 'snowflake',
-        3: 'zhaitaffy',
-    }
-
-    print('\n======================================================================')
-    response = input('Please select the resources you wish to automate:\n\n'
-                     "1. Candy Corn Gobbler\n"
-                     # "2. Snowflake Gobbler\n"
-                     # "3. Zhaitaffy Gobbler\n"
-                     "\n")
-    validResponses = '1'
-    while response not in validResponses:
-        response = input('\nInvalid selection. Enter the number corresponding to your choice. Please try again: ')
-
-    response = int(response)
-    return gobblers[response]
 
 
 def askForMethod() -> str:
     """Asks the user what method is preferred for running the program and returns it: clicks or duration."""
     methods = {
-        1: 'clicks',
-        2: 'time',
+        1: "clicks",
+        2: "time",
     }
 
-    print('\n======================================================================')
-    response = input("Would you like to automate your resources for a set number of clicks or for a set length of "
-                     "time?\n\n"
-                     "1. Number of Clicks\n"
-                     "2. Length of Time\n"
-                     "\n")
-    validResponses = ('1', '2')
+    print("\n======================================================================")
+    response = input(
+        "Would you like to automate your resources for a set number of clicks or for a set length of "
+        "time?\n\n"
+        "1. Number of Clicks\n"
+        "2. Length of Time\n"
+        "\n"
+    )
+    validResponses = ("1", "2")
     while response not in validResponses:
-        response = input('\nInvalid selection. Enter the number corresponding to your choice. Please try again: ')
+        response = input(
+            "\nInvalid selection. Enter the number corresponding to your choice. Please try again: "
+        )
 
     response = int(response)
     return methods[response]
@@ -66,18 +50,20 @@ def askForMethod() -> str:
 
 def askForNumberOfClicks() -> int:
     """Asks and returns the number of clicks the user would like to automate the resources for."""
-    print('\n======================================================================')
-    response = input("Please enter the number of clicks you'd like to run the resources for: ")
+    print("\n======================================================================")
+    response = input(
+        "Please enter the number of clicks you'd like to run the resources for: "
+    )
     validResponse = response.isdigit()
     while not validResponse:
-        response = input('\nInvalid number of clicks. Please try again: ')
+        response = input("\nInvalid number of clicks. Please try again: ")
         validResponse = response.isdigit()
 
     clicks = int(response)
     if clicks == 1:
-        clickText = 'click'
+        clickText = "click"
     else:
-        clickText = 'clicks'
+        clickText = "clicks"
     print(f"\nProgram set to run for {clicks} {clickText}!\n")
     return clicks
 
@@ -85,36 +71,42 @@ def askForNumberOfClicks() -> int:
 def askForUnitOfTime() -> str:
     """Asks and returns the unit of time the user would like to automate the resources for."""
     unitsOfTime = {
-        1: 'seconds',
-        2: 'minutes',
-        3: 'hours',
-        4: 'days',
+        1: "seconds",
+        2: "minutes",
+        3: "hours",
+        4: "days",
     }
 
-    print('\n======================================================================')
-    response = input("Please enter the unit of time you would like to run the resources for: \n\n"
-                     "1. Seconds\n"
-                     "2. Minutes\n"
-                     "3. Hours\n"
-                     "4. Days\n"
-                     "\n")
-    validResponses = ('1', '2', '3', '4')
+    print("\n======================================================================")
+    response = input(
+        "Please enter the unit of time you would like to run the resources for: \n\n"
+        "1. Seconds\n"
+        "2. Minutes\n"
+        "3. Hours\n"
+        "4. Days\n"
+        "\n"
+    )
+    validResponses = ("1", "2", "3", "4")
     while response not in validResponses:
-        response = input('\nInvalid selection. Enter the number corresponding to your choice. Please try again: ')
+        response = input(
+            "\nInvalid selection. Enter the number corresponding to your choice. Please try again: "
+        )
 
     response = int(response)
     return unitsOfTime[response]
 
 
-def askForTime() -> (int, str):
+def askForTime() -> Union(int, str):
     """Asks and returns the time the user would like to automate the resources for."""
     unitOfTime = askForUnitOfTime()
 
-    print('\n======================================================================')
-    response = input(f"Please enter how many {unitOfTime} you would like to run the resources for: \n\n")
+    print("\n======================================================================")
+    response = input(
+        f"Please enter how many {unitOfTime} you would like to run the resources for: \n\n"
+    )
     validResponse = response.isdigit()
     while not validResponse:
-        response = input('\nInvalid duration of time. Please try again: ')
+        response = input("\nInvalid duration of time. Please try again: ")
         validResponse = response.isdigit()
 
     duration = int(response)
@@ -134,13 +126,13 @@ def calculateLoopsFromTime(time: tuple[int, str]) -> int:
     """Calculates the number of loops the program will execute based on the inputted length of time."""
     duration, unit = time
     multiplier = None
-    if unit in ('seconds', 'second'):
+    if unit in ("seconds", "second"):
         multiplier = 1
-    elif unit in ('minutes', 'minute'):
+    elif unit in ("minutes", "minute"):
         multiplier = 60
-    elif unit in ('hours', 'hour'):
+    elif unit in ("hours", "hour"):
         multiplier = 60 * 60
-    elif unit in ('days', 'day'):
+    elif unit in ("days", "day"):
         multiplier = 60 * 60 * 60
 
     durationSeconds = duration * multiplier
@@ -150,35 +142,39 @@ def calculateLoopsFromTime(time: tuple[int, str]) -> int:
 
 def getGobblerLocation(gobbler: str):
     """Finds and returns the location of the resources on the main screen."""
-    gobblerImagePath = str(Path(__file__).parent / 'resources' / f'{gobbler}-gobbler.png')
+    gobblerImagePath = str(
+        Path(__file__).parent / "resources" / f"{gobbler}-gobbler.png"
+    )
 
-    print('\n======================================================================')
+    print("\n======================================================================")
     location = None
     while location is None:
-        print('Searching for resources...\n')
-        location = pyautogui.locateCenterOnScreen(gobblerImagePath, grayscale=True, confidence=0.9)
-    print('Gobbler found!')
+        print("Searching for resources...\n")
+        location = pyautogui.locateCenterOnScreen(
+            gobblerImagePath, grayscale=True, confidence=0.9
+        )
+    print("Gobbler found!")
     return location
 
 
 def clickGobbler(location, loops: int) -> None:
     """Clicks on the resources for a determined amount of time or clicks."""
-    print('\n======================================================================')
-    print('To cancel, please close this window at anytime.\n\n')
+    print("\n======================================================================")
+    print("To cancel, please close this window at anytime.\n\n")
 
     pyautogui.doubleClick(location)
     loopsRemaining = loops - 1
     loopsFinished = 1
-    print("{:.1%} completed...".format(loopsFinished / loops), end='\r')
+    print("{:.1%} completed...".format(loopsFinished / loops), end="\r")
     while loopsRemaining:
         pyautogui.sleep(5)
         pyautogui.doubleClick(location)
         loopsFinished += 1
         loopsRemaining -= 1
-        print("{:.1%} completed...".format(loopsFinished / loops), end='\r')
-    print('')
-    print('\n======================================================================')
-    print('Finished! Enjoy!')
+        print("{:.1%} completed...".format(loopsFinished / loops), end="\r")
+    print("")
+    print("\n======================================================================")
+    print("Finished! Enjoy!")
 
 
 def printCandyCornRequired(loops: int) -> None:
@@ -190,38 +186,40 @@ def printCandyCornRequired(loops: int) -> None:
     stacks, individual = divmod(totalCandy, maxCandyStack)
 
     if stacks and individual:
-        stackSuffix = '' if stacks == 1 else 's'
-        pieceSuffix = '' if individual == 1 else 's'
-        text = f"You will need {stacks} stack{stackSuffix} and {individual} piece{pieceSuffix} of candy corn in your " \
-               f"inventory."
+        stackSuffix = "" if stacks == 1 else "s"
+        pieceSuffix = "" if individual == 1 else "s"
+        text = (
+            f"You will need {stacks} stack{stackSuffix} and {individual} piece{pieceSuffix} of candy corn in your "
+            f"inventory."
+        )
     elif stacks and individual == 0:
-        stackSuffix = '' if stacks == 1 else 's'
+        stackSuffix = "" if stacks == 1 else "s"
         text = f"You will need {stacks} stack{stackSuffix} of candy corn in your inventory."
     elif stacks == 0 and individual:
-        pieceSuffix = '' if individual == 1 else 's'
+        pieceSuffix = "" if individual == 1 else "s"
         text = f"You will need {individual} piece{pieceSuffix} of candy corn in your inventory."
     else:
         text = "No candy corn is required in your inventory."
 
-    print('\n======================================================================')
+    print("\n======================================================================")
     print(text)
-    print('\n')
+    print("\n")
 
 
 def verifyUserHasCandyCorn() -> None:
     """Asks the user to confirm they have the required candy corn before proceeding."""
-    input('Press enter once you have verified you have the required amount of candy corn to commence: ')
+    input(
+        "Press enter once you have verified you have the required amount of candy corn to commence: "
+    )
 
 
 if __name__ == "__main__":
-    elevate()
 
     printInstructions()
 
-    # gobblerName = askForGobbler()
     gobblerMethod = askForMethod()
 
-    if gobblerMethod == 'clicks':
+    if gobblerMethod == "clicks":
         gobblerClicks = askForNumberOfClicks()
         gobblerLoops = calculateLoopsFromClicks(gobblerClicks)
     else:
@@ -233,5 +231,5 @@ if __name__ == "__main__":
     printCandyCornRequired(gobblerLoops)
     verifyUserHasCandyCorn()
 
-    gobblerLocation = getGobblerLocation('candy-corn')
+    gobblerLocation = getGobblerLocation("candy-corn")
     clickGobbler(gobblerLocation, gobblerLoops)
